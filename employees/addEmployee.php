657e4f2d9d863cd7cda106c60f4ae9dfe0052f6d
iamnickatuti <atutinick@gmail.com>
2023-08-25 18:56:36 +0300

    11

--- employees/addEmployee.php
+++ employees/addEmployee.php
@@ -1,25 +1,13 @@
 <?php
-session_start(); // Start session at the top
 include '../config.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Add form validation as necessary
-
     $stmt = $conn->prepare("INSERT INTO `employees` (`full_name`, `password`, `username`, `email`, `department_id`, `active`) VALUES (?, ?, ?, ?, ?, ?)");
     $stmt->bind_param("ssssii", $_POST['full_name'], password_hash($_POST['password'], PASSWORD_DEFAULT), $_POST['username'], $_POST['email'], $_POST['department_id'], $_POST['active']);
-
-    if ($stmt->execute()) {
-        $_SESSION['message'] = "Employee added successfully!";
-        $_SESSION['msg_type'] = "success";
-    } else {
-        $_SESSION['message'] = "Error adding employee!";
-        $_SESSION['msg_type'] = "danger";
-    }
-
+    $stmt->execute();
     header("Location: employees.php");
-    exit;
 }
-
 ?>
 
 <form method="post">
@@ -31,13 +19,3 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     Active: <input type="number" name="active" min="0" max="1" value="1"><br>
     <input type="submit" value="Add">
 </form>
-
-<?php
-if (isset($_SESSION['message'])) {
-    echo "<div class='alert alert-" . $_SESSION['msg_type'] . "'>" . $_SESSION['message'] . "</div>";
-    unset($_SESSION['message']);
-    unset($_SESSION['msg_type']);
-}
-?>
-
-
