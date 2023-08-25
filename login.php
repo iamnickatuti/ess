4a8ca2be3a8b05a330ccabc63385002894d0f710
iamnickatuti <atutinick@gmail.com>
2023-08-25 17:31:04 +0300

    11

--- login.php
+++ login.php
@@ -1,7 +1,5 @@
 <?php
-include './auth/process.php'
-;?>
-<?php
+include './auth/process.php';
 if (isset($_SESSION['id']))
 {?>
 <?php include './loggedin.php'?>
@@ -13,16 +11,6 @@ if (isset($_SESSION['id']))
 <head>
     <meta charset="utf-8" />
     <title>Login </title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta content="" name="description" />
-    <meta content="" name="author" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <!-- App favicon -->
-    <link rel="shortcut icon" href="assets/images/favicon.ico">
-    <!-- App css -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
-    <link href="assets/css/theme.min.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
