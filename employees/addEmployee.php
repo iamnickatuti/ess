7ed725bd90a8e54724a1b3daf86beb0f1f4407a4
iamnickatuti <atutinick@gmail.com>
2023-08-25 18:51:57 +0300

    11

--- employees/addEmployee.php
+++ employees/addEmployee.php
@@ -1,6 +1,6 @@
 <?php
 session_start(); // Start session at the top
-include 'config.php';
+include '../config.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Add form validation as necessary
@@ -41,13 +41,3 @@ if (isset($_SESSION['message'])) {
 ?>
 
 
-
-<form method="post">
-    Full Name: <input type="text" name="full_name"><br>
-    Password: <input type="password" name="password"><br>
-    Username: <input type="text" name="username"><br>
-    Email: <input type="email" name="email"><br>
-    Department ID: <input type="number" name="department_id"><br>
-    Active: <input type="number" name="active" min="0" max="1" value="1"><br>
-    <input type="submit" value="Add">
-</form>
