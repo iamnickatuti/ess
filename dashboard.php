71944ba0883a9be1655b9001d8ff043ee50ba33a
iamnickatuti <atutinick@gmail.com>
2023-08-25 18:36:27 +0300

    11

--- dashboard.php
+++ dashboard.php
@@ -7,5 +7,5 @@ if (!isset($_SESSION['id']))
 ?>
 
 <h1>Bazuu</h1>
-<a href="employees/employees.php">Employees</a>
+<a href="employees/employees.php">Employee</a>
 <a href="./auth/logout.php">Logout</a>
