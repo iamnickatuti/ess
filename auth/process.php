57f2097686fcdbdb38d7333ef6a77a1a8e41e44f
iamnickatuti <atutinick@gmail.com>
2023-08-25 18:08:40 +0300

    11

--- auth/process.php
+++ auth/process.php
@@ -1,36 +1,42 @@
 <?php
+// Turn on error reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 include './config.php';
 global $conn;
 $msg = "";
 $msg_class = "";
-if(isset($_POST['signin'])){
+
+if (isset($_POST['signin'])) {
     if (empty($_POST['unamail']) || empty($_POST['password'])) {
-        $msg = "complete fields!";
-        $msg_class="alert-danger";
-    } else{
-        $username = mysqli_real_escape_string($conn,$_POST['unamail']);
+        $msg = "Complete all fields!";
+        $msg_class = "alert-danger";
+    } else {
+        $username = mysqli_real_escape_string($conn, $_POST['unamail']);
         $password = $_POST['password'];
-        $query = "SELECT * FROM users WHERE users.username='$username'";
-        $result = $conn->query($query);
-        if ($result->num_rows<1){
+
+        $stmt = $conn->prepare("SELECT * FROM users WHERE username = ?");
+        $stmt->bind_param('s', $username);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+        if ($result->num_rows < 1) {
             $msg = "Account does not exist";
             $msg_class = "alert-danger";
-        }
-        if ($result->num_rows == 1) {
+        } elseif ($result->num_rows == 1) {
             $row = $result->fetch_array(MYSQLI_ASSOC);
-            if (!password_verify($_POST['password'], $row['password'])) {
-                $msg = "Cross-check your password!!";
+            if (!password_verify($password, $row['password'])) {
+                $msg = "Cross-check your password!";
                 $msg_class = "alert-warning";
-            }
-            else if (password_verify($_POST['password'], $row['password'])) {
-                if(is_array($row)) {
-                    $_SESSION["id"] = $row['id'];
-                }
-                if(isset($_SESSION["id"])) {
-                    header('Location:./dashboard.php');
-                }
+            } else {
+                $_SESSION["id"] = $row['id'];
+                header('Location: dashboard.php');
+                exit(); // terminate script after redirect
             }
         }
     }
 }
 
+?>
--- login.php
+++ login.php
@@ -1,46 +1,4 @@
-<?php
-session_start();
-// Turn on error reporting
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-include 'config.php';
-
-$msg = "";
-$msg_class = "";
-
-if (isset($_POST['signin'])) {
-    if (empty($_POST['unamail']) || empty($_POST['password'])) {
-        $msg = "Complete all fields!";
-        $msg_class = "alert-danger";
-    } else {
-        $username = mysqli_real_escape_string($conn, $_POST['unamail']);
-        $password = $_POST['password'];
-
-        $stmt = $conn->prepare("SELECT * FROM users WHERE username = ?");
-        $stmt->bind_param('s', $username);
-        $stmt->execute();
-
-        $result = $stmt->get_result();
-        if ($result->num_rows < 1) {
-            $msg = "Account does not exist";
-            $msg_class = "alert-danger";
-        } elseif ($result->num_rows == 1) {
-            $row = $result->fetch_array(MYSQLI_ASSOC);
-            if (!password_verify($password, $row['password'])) {
-                $msg = "Cross-check your password!";
-                $msg_class = "alert-warning";
-            } else {
-                $_SESSION["id"] = $row['id'];
-                header('Location: dashboard.php');
-                exit(); // terminate script after redirect
-            }
-        }
-    }
-}
-
-?>
+<?php include './auth/process.php'?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
